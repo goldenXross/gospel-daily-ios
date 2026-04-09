764c61fb11eac54b77d9ea88f6012ac685d5cccb
goldenXross <jesse@pixelcraftstudios.io>
2026-04-09 10:46:19 +0800

    Initial clean Xcode project

---
+++ Good News Gospel/Good News Gospel/Good_News_GospelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Good_News_GospelApp.swift
+//  Good News Gospel
+//
+//  Created by Jesse Johnson on 9/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Good_News_GospelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Good News Gospel/Good News GospelTests/Good_News_GospelTests.swift
@@ -0,0 +1,19 @@
+//
+//  Good_News_GospelTests.swift
+//  Good News GospelTests
+//
+//  Created by Jesse Johnson on 9/4/26.
+//
+
+import Testing
+@testable import Good_News_Gospel
+
+struct Good_News_GospelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
